3241848c9106e4b491f9fae90e59a12fdd77cd17
Magan-A <angel.mira@catolica.edu.sv>
2026-09-20 21:51:19 -0600

    Quitando el React donde no se usa

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bienvenida from "./components/Bienvenida";
 import Carta from "./components/Carta";
